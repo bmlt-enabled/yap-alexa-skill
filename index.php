8b0d64d052a10e18a01706610918ac53253ac558
Danny <danny.gershman@gmail.com>
2020-01-14 19:09:46 -0500

    certificate checking

--- index.php
+++ index.php
@@ -16,27 +16,32 @@ include_once 'config.php';
 include_once 'functions.php';
 
 $requestBody = file_get_contents('php://input');
-error_log($requestBody);
 if ($requestBody) {
-    $alexaRequest = Request::fromAmazonRequest($requestBody, $_SERVER['HTTP_SIGNATURECERTCHAINURL'], $_SERVER['HTTP_SIGNATURE']);
-    $validator = new RequestValidator();
-    $validator->validate($alexaRequest);
-    $responseHelper         = new ResponseHelper();
-    $requestHandlerRegistry = new RequestHandlerRegistry([
-        new HelpNeededRequestHandler($responseHelper),
-        new LaunchRequestHandler($responseHelper),
-        new FindMeetingRequestHandler($responseHelper),
-        new JustForTodayRequestHandler($responseHelper),
-        new FallbackRequestHandler($responseHelper),
-        new CancelRequestHandler($responseHelper),
-        new StopRequestHandler($responseHelper),
-        new SessionEndedRequestHandler($responseHelper)
-    ]);
-    // handle request
-    $requestHandler = $requestHandlerRegistry->getSupportingHandler($alexaRequest);
-    $response       = $requestHandler->handleRequest($alexaRequest);
-    // render response
-    header('Content-Type: application/json');
-    echo json_encode($response);
+    try {
+        $alexaRequest = Request::fromAmazonRequest($requestBody, $_SERVER['HTTP_SIGNATURECERTCHAINURL'], $_SERVER['HTTP_SIGNATURE']);
+        $validator = new RequestValidator();
+        $validator->validate($alexaRequest);
+        $responseHelper = new ResponseHelper();
+        $requestHandlerRegistry = new RequestHandlerRegistry([
+            new HelpNeededRequestHandler($responseHelper),
+            new LaunchRequestHandler($responseHelper),
+            new FindMeetingRequestHandler($responseHelper),
+            new JustForTodayRequestHandler($responseHelper),
+            new FallbackRequestHandler($responseHelper),
+            new CancelRequestHandler($responseHelper),
+            new StopRequestHandler($responseHelper),
+            new SessionEndedRequestHandler($responseHelper)
+        ]);
+        // handle request
+        $requestHandler = $requestHandlerRegistry->getSupportingHandler($alexaRequest);
+        $response = $requestHandler->handleRequest($alexaRequest);
+
+        // render response
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+    catch (Exception $ex) {
+        http_response_code(400);
+    }
 }
 exit();
